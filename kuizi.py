13040b9dc548738a066864bc78d955f82dfb485d
siliconxu <siliconxu@gmail.com>
2023-05-24 23:58:34 +0800

    bug fix

--- kuizi.py
+++ kuizi.py
@@ -23,15 +23,31 @@ class Kuizi(Plugin):
         logger.info("[Kuizi] inited")
 
     def on_handle_context(self, e_context: EventContext):
-        if e_context["context"].type != ContextType.TEXT:
+        if e_context["context"].type not in [
+            ContextType.TEXT,
+            ContextType.JOIN_GROUP,
+            ContextType.PATPAT,
+        ]:
+            return
+        
+        if e_context["context"].type == ContextType.JOIN_GROUP:
+            e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
+            return
+
+        if e_context["context"].type == ContextType.PATPAT:
+            e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
             return
 
         content = e_context["context"].content
-        content += '\n请先判断这个问题是否属于日语学习或者日本文化的范畴（不用告诉用户）。如果不属于，就委婉地拒绝用户的回答。'
-        logger.debug("[Kuizi] on_handle_context. content: %s" % content)
+        logger.debug("[Hello] on_handle_context. content: %s" % content)
+        if e_context["context"]["isgroup"]: # 只有群聊才有这个问题范畴的限制。
+            if not content.strip().startswith('fmd'): # 如果消息是以“fmd”（free mode）开头的话（不包含唤醒词），则不要对回答的范围进行
+                content += '\n请先判断这个问题是否属于日语学习或者日本文化的范畴。如果不属于，就委婉地拒绝用户的回答。不用告诉用户问题属于什么范畴。'
+                logger.debug("[Kuizi] on_handle_context. content: %s" % content)
         e_context["context"].type = ContextType.TEXT
         e_context["context"].content = content
         e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑，一般会覆写reply
+        return
 
     def get_help_text(self, **kwargs):
         help_text = "用于确保机器人不要回答跟日语学习没有关系的问题。"
